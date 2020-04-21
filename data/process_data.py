bf6c18814483b89e87f08e3f7311181575f92746
Sergey M <smouzykin@gmail.com>
2020-04-21 17:19:49 -0400

    Initial commit

---
+++ data/process_data.py
@@ -0,0 +1,124 @@
+
+"""
+MovieTweeting Data: https://github.com/sidooms/MovieTweetings/tree/master/recsyschallenge2014
+Publication: http://crowdrec2013.noahlab.com.hk/papers/crowdrec2013_Dooms.pdf
+
+"""
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+# Read in the datasets
+print("Getting data...")
+movies = pd.read_csv('https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/movies.dat',
+                     delimiter='::',
+                     header=None,
+                     names=['movie_id', 'movie', 'genre'],
+                     dtype={'movie_id': object},
+                     engine='python',
+                     encoding='utf-8')
+reviews = pd.read_csv('https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/ratings.dat',
+                      delimiter='::',
+                      header=None,
+                      names=['user_id', 'movie_id', 'rating', 'timestamp'],
+                      dtype={'movie_id': object, 'user_id': object, 'timestamp': object},
+                      engine='python',
+                      encoding='utf-8')
+
+
+# =============================================================================
+# Extract Movie Title and Year
+# =============================================================================
+print('Cleaning movies data...')
+# extract movie names, strip whitespaces
+movies['movie_name'] = movies['movie'].str.split('(', expand=True)[0].str.strip()
+
+# extract movie year
+movies['year'] = movies['movie'].str.split('(', expand=True)[1].str.strip(")")
+
+# drop original 'movie' column
+movies.drop('movie', axis=1, inplace=True)
+
+# check length of 'year', should not be longer than 4 digits
+assert (movies['year'].apply(len) > 4).sum() < 1
+
+
+# =============================================================================
+# Dummy Variables for Centuries
+# =============================================================================
+# convert dtype to integer
+movies['year'] = movies['year'].astype(np.uint16)
+
+# extract movie century
+movies_1900 = (movies['year'] >= 1900) & (movies['year'] < 2000)
+movies['2000'] = np.where(movies['year'] > 2000, 1, 0)
+movies['1900'] = np.where(movies_1900, 1, 0)
+movies['1800'] = np.where(movies['year'] < 1900, 1, 0)
+
+# check years; each row should not sum to more than 1
+assert ~any(movies[['2000', '1900', '1800']].sum(axis=1) > 1)
+
+
+# =============================================================================
+# Genres
+# =============================================================================
+
+# split genres, append to list
+genre_cat = []
+for item in movies['genre'].str.split('|'):
+    try:
+        genre_cat.extend(item)
+    except:
+        pass
+# pull out unique set of genres from list
+categories = list(set(genre_cat))
+
+
+#### Build DataFrame with Categories as Dummy Variables
+
+# DataFrame with all categories seperated
+category_df = pd.DataFrame(columns=categories, index=movies.index)
+movies = pd.concat([movies, category_df], axis=1)
+
+# convert dtype to string; avoids errors due to NaNs being floats
+movies['genre'] = movies['genre'].astype(str)
+
+# iterate over category; check if genre contains category
+for item in categories:
+    movies[item] = np.where(movies['genre'].str.contains(item), 1, 0)
+
+
+
+# =============================================================================
+# Reviews Data Processing
+# =============================================================================
+print('Cleaning reviews data...')
+reviews['timestamp'] = reviews['timestamp'].astype(np.int32)
+
+# extract date and time using 'apply'
+reviews['date'] = reviews['timestamp'].apply(lambda X: str(datetime.fromtimestamp(X)))
+reviews['year'] = reviews['timestamp'].apply(lambda X: datetime.fromtimestamp(X).year)
+reviews['month'] = reviews['timestamp'].apply(lambda X: datetime.fromtimestamp(X).month)
+
+reviews['day_of_week'] = reviews['timestamp'].apply(lambda X: datetime.fromtimestamp(X).weekday())
+reviews['day'] = reviews['timestamp'].apply(lambda X: datetime.fromtimestamp(X).day)
+
+
+
+# =============================================================================
+# Write to File
+# =============================================================================
+print("Writing clean data to file...")
+movies.to_csv("data/movies_clean.csv")
+reviews.to_csv("data/reviews_clean.csv")
+print("Finished!")
+
+
+
+
+
+
+
+
+
---
+++ recommender/recommender_functions.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+from joblib import dump
+
+
+def load_data():
+    # load moview and reviews data
+    reviews = pd.read_csv('data/reviews_clean.csv')
+    movies = pd.read_csv('data/movies_clean.csv')
+
+    return movies, reviews
+
+
+def dump_data(data, filepath):
+    try:
+        dump(data, filepath)
+    except Exception as e:
+        print(e)
+        print('Failed to pickle data')
+
+
+def create_user_item_matrix(movies, reviews):
+    # Create user-item matrix
+    usr_itm = reviews[['user_id', 'movie_id', 'rating', 'timestamp']]
+    user_item_df = usr_itm.groupby(['user_id','movie_id'])['rating'].max().unstack()
+    user_item_mat= np.array(user_item_df)
+
+    dump_data(user_item_mat, 'user_item_mat.pkl')
+
+
+
+def get_movie_names(movie_ids, movies_df):
+    '''
+    Params:
+    -------
+        movie_ids - a list of movie_ids
+        movies_df - original movies dataframe
+
+    Returns:
+        movies - a list of movie names associated with the movie_ids
+
+    '''
+    # Read in the datasets
+    movie_lst = list(movies_df[movies_df['movie_id'].isin(movie_ids)]['movie_name'])
+
+    return movie_lst
+
+
+def create_ranked_df(movies, reviews):
+        '''
+        Params:
+            movies - the movies dataframe
+            reviews - the reviews dataframe
+
+        Returns:
+            ranked_movies - a dataframe with movies that are sorted by highest avg rating, more reviews, then time, and must have more than 4 ratings
+        '''
+
+        # Pull the average ratings and number of ratings for each movie
+        movie_ratings = reviews.groupby('movie_id')['rating']
+        avg_ratings = movie_ratings.mean()
+        num_ratings = movie_ratings.count()
+        last_rating = pd.DataFrame(reviews.groupby('movie_id').max()['date'])
+        last_rating.columns = ['last_rating']
+
+        # Add Dates
+        rating_count_df = pd.DataFrame({'avg_rating': avg_ratings, 'num_ratings': num_ratings})
+        rating_count_df = rating_count_df.join(last_rating)
+
+        # merge with the movies dataset
+        movie_recs = movies.set_index('movie_id').join(rating_count_df)
+
+        # sort by top avg rating and number of ratings
+        ranked_movies = movie_recs.sort_values(['avg_rating', 'num_ratings', 'last_rating'], ascending=False)
+
+        # for edge cases - subset the movie list to those with only 5 or more reviews
+        ranked_movies = ranked_movies[ranked_movies['num_ratings'] > 4]
+
+        return ranked_movies
+
+
+def find_similar_movies(movie_id, movies_df):
+    '''
+    Params:
+        movie_id - a movie_id
+        movies_df - original movies dataframe
+
+    Returns:
+        similar_movies - an array of the most similar movies by title
+    '''
+    # dot product to get similar movies
+    movie_content = np.array(movies_df.iloc[:,4:])
+    dot_prod_movies = movie_content.dot(np.transpose(movie_content))
+
+    # find the row of each movie id
+    movie_idx = np.where(movies_df['movie_id'] == movie_id)[0][0]
+
+    # find the most similar movie indices - to start I said they need to be the same for all content
+    similar_idxs = np.where(dot_prod_movies[movie_idx] == np.max(dot_prod_movies[movie_idx]))[0]
+
+    # pull the movie titles based on the indices
+    similar_movies = np.array(movies_df.iloc[similar_idxs, ]['movie'])
+
+    return similar_movies
+
+
+def popular_recommendations(user_id, n_top, ranked_movies):
+    '''
+    Params::
+        user_id - the user_id (str) of the individual you are making recommendations for
+        n_top - an integer of the number recommendations you want back
+        ranked_movies - a pandas dataframe of the already ranked movies based on avg rating, count, and time
+
+    Returns::
+        top_movies - a list of the n_top recommended movies by movie title in order best to worst
+    '''
+
+    top_movies = list(ranked_movies['movie'][:n_top])
+
+    return top_movies
